2cd1e335b8712eb6ea64fa0a8b89ef09489396de
Joseph Lee <facebooklee52@gmail.com>
2022-01-14 18:36:32 +0800

    Minor changes

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 //Connection details
-$hostname = "sql313.epizy.com";
-$username = "epiz_26738007";
-$password = "hz3HyjMkOc3oK";
-$db_name = "epiz_26738007_cat2link";
+$hostname = "";
+$username = "";
+$password = "";
+$db_name = "";
 $content_src = "localhost";
 
 //Connect to the database
